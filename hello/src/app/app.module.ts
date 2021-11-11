a9fe9482401463a81ae51844d91b7576175fea4e
tati oliveira <tat1roliv@gmail.com>
2021-11-11 16:56:49 -0300

    folder

---
+++ hello/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { TestaComponenteComponent } from './testa-componente/testa-componente.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ hello/src/app/testa-componente/testa-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'testa-componente-component',
+    template: `
+    <p> Testa Componente no Angular </p>
+    `
+})
+
+export class TestaComponenteComponent { }
