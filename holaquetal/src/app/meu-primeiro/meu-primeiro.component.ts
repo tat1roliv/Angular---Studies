95e4778179bdfa98ab0eeacf9c5f6085987352f5
tati oliveira <tat1roliv@gmail.com>
2021-11-12 10:49:00 -0300

    hola que tal

---
+++ holaquetal/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "meu-primeiro-component",
+  template: `
+  <p>testando...</p>
+  `
+})
+
+export class MeuPrimeiroComponent { }
