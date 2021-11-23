1ac5613d23b690b488c1ccd9c1b67edefc6583b8
tati oliveira <tat1roliv@gmail.com>
2021-11-23 11:04:52 -0300

    template

--- holaquetal/src/app/cursos/cursos.component.ts
+++ holaquetal/src/app/cursos/cursos.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;//tipando a variavel (ts)
+  //nomePortal: any;//tipagem generica (ts)
+
+  cursos: string[] = [ 'Java', 'Angular', 'Typescript']
+
+  constructor() {
+    this.nomePortal = "http://www.tatioliv.live/";
+
+    /*ngFor*/
+    /*
+    for (let i = 0; i < this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }
+    */
+
+  }
 
   ngOnInit(): void {
   }
