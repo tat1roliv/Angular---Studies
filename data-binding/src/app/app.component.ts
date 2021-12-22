6866fccee9fee90e33720466792ca8de04b52923
tati oliveira <tat1roliv@gmail.com>
2021-12-22 11:06:08 -0300

    ciclo

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  /*title = 'data-binding';*/
+
+  valor: number = 5;
 }
