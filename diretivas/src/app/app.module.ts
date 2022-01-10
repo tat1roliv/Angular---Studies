1e00306e5cc590b7d9d1d813a7587c6dd5973b90
tati oliveira <tat1roliv@gmail.com>
2022-01-10 10:58:09 -0300

    ngSwitch

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponentComponent } from './teste-component/teste-component.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponentComponent
+    TesteComponentComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
