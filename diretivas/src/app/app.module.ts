cf53b41b85a5b65edc27559c58c4561a0c305eb5
tati oliveira <tat1roliv@gmail.com>
2022-01-18 10:50:45 -0300

    ng style

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponentComponent } from './teste-component/teste-component.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,13 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     TesteComponentComponent,
     NgSwitchComponent,
     DiretivaNgforComponent,
-    NgClassComponent
+    NgClassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
