660206073af6704728f53d1115b21b687832c0c9
tati oliveira <tat1roliv@gmail.com>
2022-02-21 17:40:13 -0300

    angular rotas bootstrap

---
+++ rotas-bootstrap/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+import { routing } from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
