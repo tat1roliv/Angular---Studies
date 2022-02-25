d937e86bfb2b9a868818b240bf84f7da57891bea
tati oliveira <tat1roliv@gmail.com>
2022-02-25 09:54:34 -0300

    Update curso-detalhe.component.ts

--- rotas-bootstrap/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas-bootstrap/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,13 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class CursoDetalheComponent implements OnInit {
 
   id: string = '';
+  //inscricao: Subscription;
 
   constructor(private route: ActivatedRoute) {
     //console.log(this.route);
-    this.id = this.route.snapshot.params['id'];
+    //this.id = this.route.snapshot.params['id'];
   }
 
   ngOnInit(): void {
+   this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      });
   }
 
+/*
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+*/
 }
