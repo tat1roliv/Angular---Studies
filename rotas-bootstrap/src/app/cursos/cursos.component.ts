cebe9f8c4a28245d96f80203bb97b63df2793061
tati oliveira <tat1roliv@gmail.com>
2022-03-07 10:07:11 -0300

    route query

--- rotas-bootstrap/src/app/cursos/cursos.component.ts
+++ rotas-bootstrap/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { Subscription } from 'rxjs';
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -9,11 +11,31 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[] = [];
+  pagina: number = 1;
+  //inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router,) { }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+    this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      });
   }
+
+  /*
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+*/
+
+proximaPagina(){
+  this.pagina++;
+  this.router.navigate(['/cursos'], {queryParams: {'pagina': this.pagina++}});
+}
 
 }
