c6f65d5a378387886c2fbcbf8676ac5b7b6c2387
tati oliveira <tat1roliv@gmail.com>
2022-03-08 11:59:34 -0300

    ++files

--- rotas-bootstrap/src/app/app-routing.module.ts
+++ rotas-bootstrap/src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+const appRoutes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+  },
+  {
+    path:'cursos',
+    component: CursosComponent,
+  },
+  {
+    path:'curso/:id',
+    component: CursoDetalheComponent,
+  },
+  {
+    path:'naoEcontrado',
+    component: CursoNaoEncontradoComponent,
+  }
+
+
+];
+
+export const routing : ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- rotas-bootstrap/src/app/app.module.ts
+++ rotas-bootstrap/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -24,7 +24,7 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   imports: [
     BrowserModule,
     AppRoutingModule,
-    routing
+    //routing
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
