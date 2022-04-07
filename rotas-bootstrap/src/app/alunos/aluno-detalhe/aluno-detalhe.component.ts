69372a0d1a9c482759d5caa0c2ecfe6fec4f411e
tati oliveira <tat1roliv@gmail.com>
2022-04-07 10:49:26 -0300

    clear

--- rotas-bootstrap/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas-bootstrap/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,8 +1,7 @@
-
 import { AlunosModule } from './../alunos.module';
 import { AlunosService } from './../alunos.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+//import { Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Aluno } from './../aluno';
 
@@ -11,40 +10,26 @@ import { Aluno } from './../aluno';
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.css']
 })
-export class AlunoDetalheComponent implements OnInit, OnDestroy {
+export class AlunoDetalheComponent implements OnInit {
 
-  aluno: Aluno;
-  inscricao: Subscription = new Subscription;
+  //aluno: Aluno;
+  //aluno = new Aluno();
+   //inscricao: Subscription;
+  aluno: any;
+
 
   constructor(
-    public route: ActivatedRoute,
-    public router: Router,
-    public alunosService: AlunosService
+    private route: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
   ) { }
 
   ngOnInit() {
-    /*this.inscricao = this.route.params.subscribe(
-      (params: any) => {
-        let id = params['id'];
-        this.aluno = this.alunosService.getAluno(id);
-      }
-    );*/
 
-    console.log('ngOnInit: AlunoDetalheComponent');
-
-    this.inscricao = this.route.data.subscribe(
-      (info: {aluno: Aluno}) => {
-        console.log('Recebendo o obj Aluno do resolver');
-        this.aluno = info.aluno;
-      }
+    this.route.params.subscribe(
+      (params: any) => this.aluno = this.alunosService.getAlunos()
     );
+
   }
 
-  editarContato(){
-    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
-  }
-
-  ngOnDestroy(){
-    this.inscricao.unsubscribe();
-  }
 }
